06c09d833b6b282059d235e1d102d96c8f97bd73
Dana Goosen <klymentovska@gmail.com>
2025-04-07 12:12:17 +0200

    Add rectanges to snail and player surfaces

--- runner.py
+++ runner.py
@@ -14,9 +14,12 @@ test_font = pygame.font.Font('font/pixeltype.ttf', font_size)
 sky_surface = pygame.image.load('graphics/sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 text_surface = test_font.render('My game', False, 'Black')
-snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_x_pos = 600
-snail_speed = 4
+
+snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surf.get_rect(bottomright = (600, 300))
+
+player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (80, 300))
 
 while True:
     for event in pygame.event.get():
@@ -25,11 +28,11 @@ while True:
             exit()
 
     screen.blit(sky_surface, (0, 0))
-    screen.blit(ground_surface, (0, sky_surface.get_height()))
-    screen.blit(text_surface, ((sky_surface.get_width() - text_surface.get_width()) / 2, 50))
-    screen.blit(snail_surface, (snail_x_pos, 250))
-    snail_x_pos -= snail_speed
-    if snail_x_pos < (-1)*snail_surface.get_width():
-        snail_x_pos = sky_surface.get_width() - snail_surface.get_width()
+    screen.blit(ground_surface, (0, 300))
+    screen.blit(text_surface, (300, 50))
+    screen.blit(snail_surf, snail_rect)
+    screen.blit(player_surf, player_rect)
+    snail_rect.x -= 4
+    if snail_rect.right <= 0: snail_rect.left = 800
     pygame.display.update()
     clock.tick(fps)
